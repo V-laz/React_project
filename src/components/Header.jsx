3fc2e8f672d4540f3c839dc0a5df86afd87c37b6
V-laz <Gyver2008@yandex.ru>
2022-05-02 18:21:33 +0200

    lessom 13 git

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src='https://avatars.mds.yandex.net/i?id=256178db6b9c212a7d36d5b06175bf1f-4575500-images-thumbs&n=13' alt="logo" />
+    </header>
+  )
+}
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navi">
+      <div><a>Profile </a></div>
+      <div><a>Messages</a></div>
+      <div><a>News</a></div>
+      <div><a>Music</a></div>
+      <div><a>Setting</a></div>
+    </nav>
+  )
+}
+export default NavBar;
